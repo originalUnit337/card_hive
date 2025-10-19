1fb590a49ad240e5f1758c209746a80c15f047ba
Unit337 <devguyvf@gmail.com>
2025-10-19 17:31:36 +0300

    FIX: - initial state of cardinfo was loading, now cardinfoLoaded

--- lib/features/cards/presentation/screens/card_info/card_info_screen.dart
+++ lib/features/cards/presentation/screens/card_info/card_info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:card_hive/core/ui_kit/palette/app_palette.dart';
+import 'package:card_hive/core/ui_kit/palette/palette.dart';
 import 'package:card_hive/features/cards/domain/entities/card_entity.dart';
 import 'package:card_hive/features/cards/presentation/screens/card_info/bloc/card_info_bloc.dart';
 import 'package:card_hive/features/cards/presentation/screens/card_info/bloc/card_info_state.dart';
@@ -64,108 +65,124 @@ class _CardInfoScreenState extends State<CardInfoScreen> {
           Logger().d('BUILDER WORKS');
           if (state is CardInfoLoaded) {
             currentCard = state.card;
-            return DecoratedBox(
-              decoration: BoxDecoration(color: currentPalette.background),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                spacing: 7,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(10),
-                      child: DecoratedBox(
-                        decoration: const BoxDecoration(color: Colors.white),
-                        child: Column(
-                          spacing: 12,
-                          children: [
-                            Container(
-                              height: 170,
-                              decoration: BoxDecoration(
-                                color: currentCard?.color,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              // ? logo here if exist
-                              child: Center(
-                                child: Text(
-                                  currentCard?.name ?? '',
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Image.asset('assets/bar_codes/test_image.png'),
-                            Center(
-                              child: Text(
-                                currentCard?.number ?? '',
-                                style: const TextStyle(fontSize: 38),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsetsGeometry.only(left: 15, right: 15),
-                    child: ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(10),
-                      child: Material(
-                        child: ListTile(
-                          leading: const Icon(Icons.photo_camera),
-                          title: Text(
-                            'Card Pictures',
-                            style: TextStyle(
-                              color: currentPalette.primary,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onTap:
-                              () =>
-                                  context.push(AppRoutes.cardInfoPictures.path),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsetsGeometry.only(left: 15, right: 15),
-                    child: ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(10),
-                      child: Material(
-                        child: InkWell(
-                          hoverDuration: const Duration(milliseconds: 1000),
-                          hoverColor: Colors.yellow,
-                          focusColor: Colors.purple,
-                          splashColor: Colors.green,
-                          highlightColor: Colors.limeAccent,
-                          child: ListTile(
-                            leading: const Icon(Icons.notes),
-                            title: Text(
-                              'Notes',
-                              style: TextStyle(
-                                color: currentPalette.primary,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          onTap:
-                              () => context.pushNamed(
-                                AppRoutes.cardInfoNotes.name,
-                              ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            return _BuildInfoScreen(
+              currentPalette: currentPalette,
+              currentCard: currentCard,
             );
           } else {
-            return const CircularProgressIndicator();
+            return _BuildInfoScreen(
+              currentPalette: currentPalette,
+              currentCard: currentCard,
+            );
           }
         },
+      ),
+    );
+  }
+}
+
+class _BuildInfoScreen extends StatelessWidget {
+  const _BuildInfoScreen({
+    required this.currentPalette,
+    required this.currentCard,
+  });
+
+  final Palette currentPalette;
+  final CardEntity? currentCard;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: currentPalette.background),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        spacing: 7,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(10),
+              child: DecoratedBox(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: Column(
+                  spacing: 12,
+                  children: [
+                    Container(
+                      height: 170,
+                      decoration: BoxDecoration(
+                        color: currentCard?.color,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      // ? logo here if exist
+                      child: Center(
+                        child: Text(
+                          currentCard?.name ?? '',
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Image.asset('assets/bar_codes/test_image.png'),
+                    Center(
+                      child: Text(
+                        currentCard?.number ?? '',
+                        style: const TextStyle(fontSize: 38),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsGeometry.only(left: 15, right: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(10),
+              child: Material(
+                child: ListTile(
+                  leading: const Icon(Icons.photo_camera),
+                  title: Text(
+                    'Card Pictures',
+                    style: TextStyle(
+                      color: currentPalette.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  onTap: () => context.push(AppRoutes.cardInfoPictures.path),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsGeometry.only(left: 15, right: 15),
+            child: ClipRRect(
+              borderRadius: BorderRadiusGeometry.circular(10),
+              child: Material(
+                child: InkWell(
+                  hoverDuration: const Duration(milliseconds: 1000),
+                  hoverColor: Colors.yellow,
+                  focusColor: Colors.purple,
+                  splashColor: Colors.green,
+                  highlightColor: Colors.limeAccent,
+                  child: ListTile(
+                    leading: const Icon(Icons.notes),
+                    title: Text(
+                      'Notes',
+                      style: TextStyle(
+                        color: currentPalette.primary,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  onTap: () => context.pushNamed(AppRoutes.cardInfoNotes.name),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
